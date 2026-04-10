034462e5f4dbaa493e6aa093c7d789917c9cd9aa
= <=>
2026-04-11 02:23:03 +0300

    Gi

--- frontend/ts/ui/mobileMenu.ts
+++ frontend/ts/ui/mobileMenu.ts
@@ -2,8 +2,14 @@ export default () => document.addEventListener('DOMContentLoaded', () => {
     const mobileMenu = document.querySelector<HTMLElement>('.mobile-menu');
     if (!mobileMenu) return;
 
-    const toggleMenu = function() {
-        mobileMenu.classList.toggle('--opened');
+    const toggleMenu = function(open: true | false | 'toggle' = 'toggle') {
+        if (open === 'toggle') {
+            mobileMenu.classList.toggle('--opened');
+        } else if (open === true) {
+            mobileMenu.classList.add('--opened');
+        } else {
+            mobileMenu.classList.remove('--opened');
+        }
 
         const menuBtns = document.querySelectorAll('.actionOpenMenu');
 
@@ -18,6 +24,12 @@ export default () => document.addEventListener('DOMContentLoaded', () => {
 
         if ((ev.target as HTMLElement).closest('.actionOpenMenu')) {
             toggleMenu();
+        }
+    });
+
+    mobileMenu.addEventListener('click', ev => {
+        if ((ev.target as Element).closest('a')) {
+            toggleMenu(false);
         }
     });
 });
